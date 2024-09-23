789934f9ea10ed1f363ba95472d280e8e1317eac
Steffan Andrews <stef@orchetect.com>
2024-09-22 18:59:55 -0700

    Cleanup

--- Package.swift
+++ Package.swift
@@ -5,22 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "TextFileKit",
-    
     platforms: [
         .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
-    
     products: [
         .library(
             name: "TextFileKit",
             targets: ["TextFileKit"]
         )
     ],
-    
-    dependencies: [
-        // none
-    ],
-    
     targets: [
         .target(
             name: "TextFileKit",
