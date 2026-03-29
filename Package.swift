827d75b072df015c844fa61dd2544f27b5655691
Steffan Andrews <stef@orchetect.com>
2026-03-29 15:13:13 -0700

    Bumped package toolchain to Swift 6.0

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
     name: "swift-textfile",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4)
     ],
     products: [
         .library(name: "TextFile", targets: ["TextFile"])
