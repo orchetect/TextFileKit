9d1a072c8954a17c70d2516aafcbc746f420a49f
Steffan Andrews <stef@orchetect.com>
2026-03-28 20:44:38 -0700

    Improved text encoding auto-detection heuristic

--- Sources/TextFile/TextFileDecodingStrategy/Strategies/HybridTextFileDecodingStrategy.swift
+++ Sources/TextFile/TextFileDecodingStrategy/Strategies/HybridTextFileDecodingStrategy.swift
@@ -64,12 +64,38 @@ extension HybridTextFileDecodingStrategy {
     /// Main hybrid decoding trunk method.
     func decodeTextHybrid(in data: Data, fileURL: URL?) throws(TextFileDecodeError) -> DecodedTextFile {
         // Step 1: try detecting a BOM, if present
-        // UTF-8 with BOM will be decoded by the auto-detecting API after this step, so ignore it at this stage
-        if let decoded = try decodeTextUsingBOM(ignoring: [.utf8], in: data, fileURL: fileURL) {
+        if let decoded = try decodeTextUsingBOM(in: data, fileURL: fileURL) {
             return decoded
         }
         
-        // Step 2: attempt to detect encoding if BOM is not present
+        // Step 2: try UTF-8 without BOM
+        if let decoded = try decodeUTF8(in: data, fileURL: fileURL) {
+            return decoded
+        }
+        
+        // Step 3: attempt to detect UTF-16 / UTF-32 that may be missing a BOM
+        if let decoded = try decodeTextGuessingMultiByteUTFEncoding(in: data, fileURL: fileURL) {
+            return decoded
+        }
+        
+        // Step 3:
+        // once in a rare while, a BOM is inserted at the start of a file that it doesn't
+        // belong in. we can try decoding the file once more after stripping out the BOM.
+        if let decoded = try decodeTextAutomaticallyAfterRemovingBOM(in: data, fileURL: fileURL) {
+            return decoded
+        }
+        
+        // Step 4: try ISO-8859-1 (ISO Latin-1)
+        if let decoded = try decodeISOLatin1(in: data, fileURL: fileURL) {
+            return decoded
+        }
+        
+        // Step 5: try Windows-12520 or MacRoman
+        if let decoded = try decodeWindows1252OrMacRoman(in: data, fileURL: fileURL) {
+            return decoded
+        }
+        
+        // Step 6: attempt to detect encoding if BOM is not present
         var heldError: TextFileDecodeError? = nil
         do throws(TextFileDecodeError) {
             let decoded = try decodeTextAutomatically(allowLossy: false, in: data, fileURL: fileURL)
@@ -78,19 +104,7 @@ extension HybridTextFileDecodingStrategy {
             heldError = error
         }
         
-        // Step 3: attempt to detect UTF-16 / UTF-32 that may be missing a BOM
-        if let decoded = try decodeTextGuessingMultiByteUTFEncoding(in: data, fileURL: fileURL) {
-            return decoded
-        }
-        
-        // Step 4:
-        // once in a rare while, a BOM is inserted at the start of a file that it doesn't
-        // belong in. we can try decoding the file once more after stripping out the BOM.
-        if let decoded = try decodeTextAutomaticallyAfterRemovingBOM(in: data, fileURL: fileURL) {
-            return decoded
-        }
-        
-        // Step 5: all options have been exhausted, so throw an error
+        // Step 7: all options have been exhausted, so throw an error
         throw heldError ?? .unrecognizedTextEncoding
     }
 
@@ -100,10 +114,10 @@ extension HybridTextFileDecodingStrategy {
         fileURL: URL?
     ) throws(TextFileDecodeError) -> DecodedTextFile? {
         guard let encoding,
-              let rawValue = String(data: data, encoding: encoding)
+              let text = String(data: data, encoding: encoding)
         else { return nil }
         
-        return DecodedTextFile(content: rawValue, encoding: encoding, url: fileURL)
+        return DecodedTextFile(content: text, encoding: encoding, url: fileURL)
     }
     
     func decodeTextUsingBOM(
@@ -117,10 +131,18 @@ extension HybridTextFileDecodingStrategy {
         guard !ignoredBOMs.contains(bom)
         else { return nil }
         
-        guard let rawValue = String(data: data, encoding: bom.encoding)
+        let sourceData: Data
+        if bom == .utf8, data.count >= ByteOrderMark.utf8.bytes.count {
+            // strip UTF-8 BOM because String decoding API doesn't like it
+            sourceData = data[data.startIndex.advanced(by: ByteOrderMark.utf8.bytes.count)...]
+        } else {
+            sourceData = data
+        }
+        
+        guard let text = String(data: sourceData, encoding: bom.encoding)
         else { return nil }
         
-        return DecodedTextFile(content: rawValue, encoding: bom.encoding, url: fileURL)
+        return DecodedTextFile(content: text, encoding: bom.encoding, url: fileURL)
     }
     
     func decodeTextAutomatically(
@@ -143,10 +165,10 @@ extension HybridTextFileDecodingStrategy {
         fileURL: URL?
     ) throws(TextFileDecodeError) -> DecodedTextFile? {
         guard let possibleEncoding = data.guessMultiByteUTFEncoding(),
-              let rawValue = String(data: data, encoding: possibleEncoding)
+              let text = String(data: data, encoding: possibleEncoding)
         else { return nil }
         
-        return DecodedTextFile(content: rawValue, encoding: possibleEncoding, url: fileURL)
+        return DecodedTextFile(content: text, encoding: possibleEncoding, url: fileURL)
     }
     
     func decodeTextAutomaticallyAfterRemovingBOM(
@@ -163,6 +185,122 @@ extension HybridTextFileDecodingStrategy {
         
         decoded.url = fileURL
         return decoded
+    }
+    
+    func decodeUTF8(
+        in data: Data,
+        fileURL: URL?
+    ) throws(TextFileDecodeError) -> DecodedTextFile? {
+        guard let text = String(data: data, encoding: .utf8) else {
+            return nil
+        }
+        return DecodedTextFile(content: text, encoding: .utf8, url: fileURL)
+    }
+    
+    // ISO-8859-1 (isoLatin1 case of String.Encoding)
+    //
+    // The ASCII table, when defined according to the ISO-8859-1 character encoding (also known
+    // as iso-ir-100, csISOLatin1, latin1, l1, IBM819, CP819), includes ASCII control characters
+    // and ASCII printable characters. Moreover, it also includes the extended ASCII character
+    // set unique to ISO-8859-1. This character set is particularly designed to support
+    // Latin1/Western European languages.
+    //
+    // See https://stackoverflow.com/a/64276978/2805570
+    func decodeISOLatin1(
+        in data: Data,
+        fileURL: URL?
+    ) throws(TextFileDecodeError) -> DecodedTextFile? {
+        // It's not trivial to do binary regex in Swift, so since we're only looking for byte ranges
+        // we can use a non-regex solution to detect or count bytes in certain ranges.
+        
+        // let notISO8859 = #"[\x00-\x06\x0B\x0E-\x1A\x1C-\x1F\x7F\x80-\x84\x86-\x9F]"#
+        let byteRangesNotUsedInISO8859: [ClosedRange<UInt8>] = [
+            0x00 ... 0x06, 0x0B ... 0x0B, 0x0E ... 0x1A, 0x1C ... 0x1F, 0x7F ... 0x7F, 0x80 ... 0x84, 0x86 ... 0x9F
+        ]
+        
+        // if any of these bytes are present, it's not ISO-8859-1
+        guard !data.contains(anyElementsIn: byteRangesNotUsedInISO8859) else {
+            return nil
+        }
+        
+        // try decoding
+        guard let text = String(data: data, encoding: .isoLatin1) else {
+            return nil
+        }
+        
+        return DecodedTextFile(content: text, encoding: .isoLatin1, url: fileURL)
+    }
+    
+    /// # How do you distinguish MacRoman from CP1252?
+    ///
+    /// # Undefined characters
+    ///
+    /// The bytes 0x81, 0x8D, 0x8F, 0x90, 0x9D are not used in CP1252. If they occur, then assume the data is MacRoman.
+    ///
+    /// # Identical characters
+    ///
+    /// The bytes 0xA2 (`¢`), 0xA3 (`£`), 0xA9 (`©`), 0xB1 (`±`), 0xB5 (`µ`) happen to be the same in both encodings.
+    /// If these are the only non-ASCII bytes, then it doesn't matter whether you choose MacRoman or CP1252.
+    ///
+    /// # Statistical approach
+    ///
+    /// Count character (NOT byte!) frequencies in the data you know to be UTF-8.
+    /// Determine the most frequent characters.
+    /// Then use this data to determine whether the CP1252 or MacRoman characters are more common.
+    ///
+    /// For example, in a search I just performed on 100 random English Wikipedia articles,
+    /// the most common non-ASCII characters are `·•–é°®’èö—`. Based on this fact,
+    ///
+    /// - The bytes 0x92, 0x95, 0x96, 0x97, 0xAE, 0xB0, 0xB7, 0xE8, 0xE9, or 0xF6 suggest Windows-1252.
+    /// - The bytes 0x8E, 0x8F, 0x9A, 0xA1, 0xA5, 0xA8, 0xD0, 0xD1, 0xD5, or 0xE1 suggest MacRoman.
+    ///
+    /// Count up the CP1252-suggesting bytes and the MacRoman-suggesting bytes, and go with whichever is greatest.
+    ///
+    /// See https://stackoverflow.com/a/4200765/2805570
+    func decodeWindows1252OrMacRoman(
+        in data: Data,
+        fileURL: URL?
+    ) throws(TextFileDecodeError) -> DecodedTextFile? {
+        // It's not trivial to do binary regex in Swift, so since we're only looking for byte ranges
+        // we can use a non-regex solution to detect or count bytes in certain ranges.
+        
+        // let notIn1BYTE = #"[\x00-\x06\x0B\x0E-\x1A\x1C-\x1F\x7F]"#
+        let byteRangesNotIn1Byte: [ClosedRange<UInt8>] = [
+            0x00 ... 0x06, 0x0B ... 0x0B, 0x0E ... 0x1A, 0x1C ... 0x1F, 0x7F ... 0x7F
+        ]
+        
+        // if any of these bytes are present, it's neither CP1252 nor MacRoman
+        guard !data.contains(anyElementsIn: byteRangesNotIn1Byte) else {
+            return nil
+        }
+        
+        // let notInCP1252 = #"[\x81\x8D\x8F\x90\x9D]"#
+        let bytesNotInCP1252: [UInt8] = [0x81, 0x8D, 0x8F, 0x90, 0x9D]
+        
+        // if any of these bytes are present, it's not CP1252
+        let isNotCP1252 = data.contains(anyElementsIn: bytesNotInCP1252)
+        
+        // let cp1252CharBytes = #"[\x92\x95\x96\x97\xAE\xB0\xB7\xE8\xE9\xF6]"#
+        let cp1252CharBytes: [UInt8] = [0x92, 0x95, 0x96, 0x97, 0xAE, 0xB0, 0xB7, 0xE8, 0xE9, 0xF6]
+        let cp1252CharBytesCount = data.count(ofElementsIn: cp1252CharBytes)
+        
+        // let macRomanCharBytes = #"[\x8E\x8F\x9A\xA1\xA5\xA8\xD0\xD1\xD5\xE1]"#
+        let macRomanCharBytes: [UInt8] = [0x8E, 0x8F, 0x9A, 0xA1, 0xA5, 0xA8, 0xD0, 0xD1, 0xD5, 0xE1]
+        let macRomanCharBytesCount = data.count(ofElementsIn: macRomanCharBytes)
+        
+        if !isNotCP1252, (macRomanCharBytesCount < cp1252CharBytesCount) {
+            // Likely Windows-1252
+            guard let text = String(data: data, encoding: .windowsCP1252) else {
+                return nil
+            }
+            return DecodedTextFile(content: text, encoding: .windowsCP1252, url: fileURL)
+        } else {
+            // MacRoman
+            guard let text = String(data: data, encoding: .macOSRoman) else {
+                return nil
+            }
+            return DecodedTextFile(content: text, encoding: .macOSRoman, url: fileURL)
+        }
     }
 }
 
