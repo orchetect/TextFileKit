d74e6979f441cc744d93c31134ab9d19250d5f1a
Steffan Andrews <stef@orchetect.com>
2026-03-29 01:54:17 -0700

    Added `TextFileEncodeError`

---
+++ Sources/TextFile/Errors/TextFileEncodeError.swift
@@ -0,0 +1,34 @@
+//
+//  TextFileEncodeError.swift
+//  swift-textfile • https://github.com/orchetect/swift-textfile
+//  © 2018-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import protocol Foundation.LocalizedError
+
+public enum TextFileEncodeError: LocalizedError {
+    case encodingFailed(underlyingError: Error?)
+    case fileWriteError(underlyingError: Error?)
+    
+    public var errorDescription: String? {
+        switch self {
+        case let .encodingFailed(underlyingError):
+            var string = "Failed to encode text: "
+            if let underlyingError {
+                string += ": \(underlyingError.localizedDescription)"
+            } else {
+                string += "."
+            }
+            return string
+            
+        case let .fileWriteError(underlyingError):
+            var string = "File write error"
+            if let underlyingError {
+                string += ": \(underlyingError.localizedDescription)"
+            } else {
+                string += "."
+            }
+            return string
+        }
+    }
+}
